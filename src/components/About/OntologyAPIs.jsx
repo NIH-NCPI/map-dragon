18031053fafd4fa14f878571252979cde9494e43
Yelena Cox <yelenacox@gmail.com>
2024-08-23 20:54:16 -0500

    Added filter functionality to ontologyAPI table for ontologies and curies

--- src/components/About/OntologyAPIs.jsx
+++ src/components/About/OntologyAPIs.jsx
@@ -2,99 +2,60 @@ import { useContext, useEffect, useState } from 'react';
 import './OntologyInfo.scss';
 import { getOntologies } from '../Manager/FetchManager';
 import { myContext } from '../../App';
-import { Spinner } from '../Manager/Spinner';
-import { Button, Input, Space, Table } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import { SmallSpinner, Spinner } from '../Manager/Spinner';
+
+import { OntologyTable } from './OntologyTable';
 
 export const OntologyInfo = () => {
   const { vocabUrl } = useContext(myContext);
-  const [ontologies, setOntologies] = useState([]);
+  const [ontologyApis, setOntologyApis] = useState([]);
+  const [ontology, setOntology] = useState([]);
   const [active, setActive] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [tableLoading, setTableLoading] = useState(false);
 
+  // Gets the ontologyAPIs on first load, automatically sets active to the first of the list to display on the page
   useEffect(() => {
     setLoading(true);
     getOntologies(vocabUrl)
       .then(data => {
-        setOntologies(data);
+        setOntologyApis(data);
         if (data.length > 0) {
           setActive(data[0]?.api_id);
         }
       })
       .finally(() => setLoading(false));
-  }, [vocabUrl]);
+  }, []);
 
+  // Fetches the active ontologyAPI each time the active API changes
   useEffect(() => {
-    if (ontologies.length > 0) {
-      setActive(ontologies[0]?.api_id);
-    }
-  }, [ontologies]);
+    active && getOntologyApiById();
+  }, [active]);
 
-  const columns = [
-    {
-      title: 'Ontology',
-      dataIndex: 'ontology',
-      filterDropdown: ({
-        setSelectedKeys,
-        selectedKeys,
-        confirm,
-        clearFilters,
-      }) => (
-        <div style={{ padding: 8 }}>
-          <Input
-            placeholder={`Search Ontology`}
-            value={selectedKeys[0]}
-            onChange={e => {
-              setSelectedKeys(e.target.value ? [e.target.value] : []);
-              confirm({ closeDropdown: false });
-            }}
-            style={{ display: 'block', marginBottom: 8 }}
-          />
-          <Space>
-            <Button
-              onClick={() => {
-                clearFilters();
-                setSelectedKeys([]);
-                confirm({ closeDropdown: false });
-              }}
-              size="small"
-              style={{ width: 90 }}
-            >
-              Reset
-            </Button>
-          </Space>
-        </div>
-      ),
-      onFilter: (value, record) =>
-        record.ontology.toString().toLowerCase().includes(value.toLowerCase()),
-      filterIcon: filtered => (
-        <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
-      ),
-      width: 400,
-    },
-
-    {
-      title: 'Curie',
-      dataIndex: 'curie',
-      width: 100,
-    },
-    {
-      title: 'Version',
-      dataIndex: 'version',
-      width: 200,
-    },
-  ];
-
-  const dataSource = ontologies
-    .filter(api => api.api_id === active)
-    .flatMap(item =>
-      Object.values(item?.ontologies).map((ont, i) => ({
-        key: i,
-        ontology: ont.ontology_title,
-        curie: ont.curie,
-        version: ont.version,
-      }))
+  const getOntologyApiById = () => {
+    return (
+      setTableLoading(true),
+      fetch(`${vocabUrl}/OntologyAPI/${active}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            return res.json().then(error => {
+              throw new Error(error);
+            });
+          }
+        })
+        .then(data => {
+          setOntology(data);
+        })
+        .finally(() => setTableLoading(false))
     );
+  };
 
   return loading ? (
     <Spinner />
@@ -105,7 +66,7 @@ export const OntologyInfo = () => {
       <div className="ontology_container">
         <div className="api_list">
           <div className="api_label">APIs</div>
-          {ontologies.map((api, index) => (
+          {ontologyApis.map((api, index) => (
             <div
               key={index}
               className={active === api.api_id ? 'active_api' : 'inactive_api'}
@@ -116,16 +77,11 @@ export const OntologyInfo = () => {
           ))}
         </div>
         <div className="ontology_list">
-          <Table
-            columns={columns}
-            dataSource={dataSource}
-            pagination={{
-              pageSize: 50,
-            }}
-            scroll={{
-              y: 470,
-            }}
-          />
+          {tableLoading ? (
+            <SmallSpinner />
+          ) : (
+            <OntologyTable ontology={ontology} />
+          )}
         </div>
       </div>
     </div>
