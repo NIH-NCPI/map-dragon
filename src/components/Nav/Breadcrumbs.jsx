b3c61bc0d4a0c87b051dbe0e4f63f51473bf23ec
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2024-10-23 15:46:47 -0500

    using pathname for labels

--- src/components/Nav/Breadcrumbs.jsx
+++ src/components/Nav/Breadcrumbs.jsx
@@ -1,3 +1,16 @@
-export const Breadcrumbs = () =>{
-   return <h1>sup breadcrumb</h1>
+import { Breadcrumb } from "antd"
+import { useLocation } from 'react-router-dom';
+export const Breadcrumbs = () => {
+   const location = useLocation();
+   const pathParts = location.pathname.split('/').filter(Boolean);
+   const pathArr = []
+   pathParts.forEach(path => {
+      pathArr.push({title:path});
+   });
+   return <span className="breadcrumbs">
+      <Breadcrumb
+         items={pathArr}
+         params={{ id: 1 }}
+      />
+   </span>
 }
