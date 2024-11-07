a9bcc32a31e7b8471a71d23eb6b396652a1c0e78
Yelena Cox <yelenacox@gmail.com>
2024-11-07 14:59:27 -0600

    Started working on pref. terminology functionality in get mappings button

--- src/components/Projects/Terminologies/Terminology.jsx
+++ src/components/Projects/Terminologies/Terminology.jsx
@@ -28,6 +28,7 @@ import { PreferredTerminology } from './PreferredTerminology';
 import { SearchContext } from '../../../Contexts/SearchContext';
 import { FilterSelect } from '../../Manager/MappingsFunctions/FilterSelect';
 import { cleanedName } from '../../Manager/Utilitiy';
+import { AssignMappingsViaButton } from './AssignMappingsViaButton';
 
 export const Terminology = () => {
   const [form] = Form.useForm();
@@ -43,12 +44,12 @@ export const Terminology = () => {
     setGetMappings,
     mapping,
     setMapping,
-    setAssignMappings,
   } = useContext(MappingContext);
 
   const [pageSize, setPageSize] = useState(
     parseInt(localStorage.getItem('pageSize'), 10) || 10
   );
+  const [assignMappingsViaButton, setAssignMappingsViaButton] = useState(false);
   const handleTableChange = (current, size) => {
     setPageSize(size);
   };
@@ -62,6 +63,8 @@ export const Terminology = () => {
     },
     []
   );
+
+  console.log(prefTerminologies);
 
   const [loading, setLoading] = useState(true);
   const initialTerminology = { url: '', description: '', name: '', codes: [] }; //initial state of terminology
@@ -121,13 +124,23 @@ It then shows the mappings as table data and alows the user to delete a mapping 
   const noMapping = variable => (
     <Button
       onClick={() => {
-        setGetMappings({ display: variable.display, code: variable.code });
+        prefTerminologies.length > 0
+          ? setAssignMappingsViaButton({
+              display: variable.display,
+              code: variable.code,
+            })
+          : setGetMappings({
+              display: variable.display,
+              code: variable.code,
+            });
       }}
     >
-      {prefTerminologies.length > 0 ? 'Assign Mappings' : 'Get Mappings'}
+      {prefTerminologies && prefTerminologies.length > 0
+        ? 'Assign Mappings'
+        : 'Get Mappings'}
     </Button>
   );
-  console.log(prefTerminologies);
+
   const matchCode = variable => {
     if (!mapping?.length) {
       return noMapping(variable);
@@ -141,7 +154,9 @@ It then shows the mappings as table data and alows the user to delete a mapping 
       return variableMappings.mappings.map(code => (
         <div className="mapping" key={code.display}>
           <span className="mapping-display">
-            <Tooltip title={code.code}>{code.display}</Tooltip>
+            <Tooltip title={code.code}>
+              {code.display ? code.display : code.code}
+            </Tooltip>
           </span>
           <span
             className="remove-mapping"
@@ -433,6 +448,11 @@ It then shows the mappings as table data and alows the user to delete a mapping 
           <LoadCodes
             terminology={terminology}
             setTerminology={setTerminology}
+          />
+          <AssignMappingsViaButton
+            assignMappingsViaButton={assignMappingsViaButton}
+            setAssignMappingsViaButton={setAssignMappingsViaButton}
+            terminology={terminology}
           />
         </div>
       )}
