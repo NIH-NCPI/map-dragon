840d63405f8570ab1bca3c5d2ac02b890b0878fc
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2024-12-10 12:07:34 -0600

    get status button (dev)

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
   });
 
   useEffect(() => {
-    getSessionStatus(vocabUrl);
+    // getSessionStatus(vocabUrl);
   });
   return (
     <GoogleOAuthProvider clientId={clientId}>
--- src/components/Auth/LoginPage.jsx
+++ src/components/Auth/LoginPage.jsx
@@ -1,14 +1,15 @@
 import { Login } from "./Login"
 import { useContext } from 'react';
 import { myContext } from '../../App';
+import { getSessionStatus } from "../Manager/SessionsManager";
 export const LoginPage = () => {
-    const {user} = useContext(myContext);
+    const {user,vocabUrl} = useContext(myContext);
     return <div style={{background:"rgba(0,0,0,.2"}}>
         {user &&
         <h2 style={{textAlign:"center",paddingTop:'5rem'}}>You are logged in and can navigate to the rest of the site</h2>}
         <div style={{ height:"5rem",display:"flex",flexDirection:"column",justifyContent:"space-around",alignItems:"center"}}>
             <Login />
         </div>
-        
+        <button onClick={()=>getSessionStatus(vocabUrl)}>check status</button>
     </div>
 }
