0bcf54a896598fcb3d9e14bf2df86a4826efbf30
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2024-12-10 12:07:34 -0600

    get status button (dev)

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
     top: '25vh',
   });
   useEffect(() => {
-    getSessionStatus(vocabUrl);
+    // getSessionStatus(vocabUrl);
   });
   return (
     <GoogleOAuthProvider clientId={clientId}>
