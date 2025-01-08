511088d0c3353c491d579b1516a46274f94eb0ba
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2025-01-08 08:56:09 -0600

    C2save

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
   });
 
   useEffect(() => {
-    // getSessionStatus(vocabUrl);
+    getSessionStatus(vocabUrl);
   });
   return (
     <GoogleOAuthProvider clientId={clientId}>
