012e7b660855189fcbe04d9e1e46462db6f2a1e0
Yelena Cox <yelenacox@gmail.com>
2025-04-24 14:43:42 -0500

    Version content in footer

--- src/components/Nav/Footer.jsx
+++ src/components/Nav/Footer.jsx
@@ -1,5 +1,37 @@
+import { notification } from 'antd';
+import { useContext, useEffect } from 'react';
 import './NavBar.scss';
+import { myContext } from '../../App';
+import { getAll } from '../Manager/FetchManager';
 
 export const Footer = () => {
-  return <footer className="footer"></footer>;
+  const { version, setVersion, mapDragonVersion, vocabUrl } =
+    useContext(myContext);
+
+  useEffect(() => {
+    getAll(vocabUrl, 'version', null)
+      .then(data => setVersion(data))
+      .catch(error => {
+        if (error) {
+          notification.error({
+            message: 'Error',
+            description: 'An error occurred loading the version.',
+          });
+        }
+        return error;
+      });
+  }, []);
+
+  return (
+    <footer className="footer">
+      <div className="footer_content_wrapper">
+        <div className="footer_content">
+          Locutus version: {version?.version}
+        </div>
+        <div className="footer_content">
+          Map Dragon version: {mapDragonVersion}
+        </div>
+      </div>
+    </footer>
+  );
 };
