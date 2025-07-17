3b028ed164d0a136ac07a80a7c9e8ad68c42b6c0
Yelena Cox <yelenacox@gmail.com>
2025-07-17 11:41:06 -0500

    Removing extra comments

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: false, // ← disables minification
-    sourcemap: true, // ← generates source maps
+    minify: false,
+    sourcemap: true,
   },
 });
