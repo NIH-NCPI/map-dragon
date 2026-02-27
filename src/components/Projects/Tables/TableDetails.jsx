d16e4654c63556c216dcb3519add2012bac6e079
Yelena Cox <yelenacox@gmail.com>
2026-02-27 13:29:33 -0600

    Styling for navbar

--- src/components/Projects/Tables/TableDetails.jsx
+++ src/components/Projects/Tables/TableDetails.jsx
@@ -560,7 +560,7 @@ It then shows the mappings as table data and alows the user to delete a mapping 
                 <Form form={form}>
                   <Table
                     scroll={{ x: 'max-content' }}
-                    sticky={{ offsetHeader: 135 }}
+                    sticky={{ offsetHeader: 80 }}
                     columns={columns}
                     dataSource={dataSource}
                     expandable={{
