5340a63382560a6f90368ccc01e31cfcd0799cc2
yelenacox <yelenacox@gmail.com>
2026-03-26 14:48:56 -0500

    Removing mapping relationship from mapped_term column

--- src/components/Projects/Tables/TableDetails.jsx
+++ src/components/Projects/Tables/TableDetails.jsx
@@ -477,7 +477,7 @@ It then shows the mappings as table data and alows the user to delete a mapping 
             >
               {code?.ftd_code}
             </span>
-            {code?.display && ` - ${code?.display}`} {relationshipDisplay(code)}
+            {code?.display && ` - ${code?.display}`}
           </span>
           <span
             className="mapping_actions"
