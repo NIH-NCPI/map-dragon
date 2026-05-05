3a49c81116d128dd36790a2945196fbfcfe754a7
yelenacox <yelenacox@gmail.com>
2026-05-05 10:35:31 -0500

    Passed setExistingMappings to ResetMappings

--- src/components/Manager/MappingsFunctions/EditMappingModal.jsx
+++ src/components/Manager/MappingsFunctions/EditMappingModal.jsx
@@ -179,6 +179,7 @@ export const EditMappingsModal = ({
                       setMappingsForSearch([]);
                     }
                   }}
+                  setExistingMappings={setExistingMappings}
                   form={form}
                 />
               )}
